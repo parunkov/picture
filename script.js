dbcd12e3186ddac4c4869012ee67894cc30620ad
parunkov <parunkov@gmail.com>
2019-12-06 14:49:22 +0500

    сделал картинку

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-console.log('!!!');
 (function() {
 	let picture = document.querySelector('.wp-image-3795');
 	let blockleft = document.querySelector('.blockleft');
-	console.log(picture.src);
+	let style = 'position: absolute; top: -100px; left:90px; width: 912px; height: 480px; background: url(' + picture.src + ') no-repeat; background-size: contain;';
 	picture.addEventListener('click', function(evt) {
-		console.log(4);
+		evt.preventDefault();
 		let pictureBig = document.createElement('div');
 		pictureBig.className = 'picture-big';
 		blockleft.prepend(pictureBig);
+		blockleft.setAttribute('style', 'position: relative;');
+		pictureBig.setAttribute('style', style);
+		let popupClose = document.createElement('div');
+		popupClose.className = 'popup-close';
+		popupClose.innerHTML = 'Закрыть Х';
+		
 		
 	});
 })();

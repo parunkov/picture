e7b91e34d82e31572c27836e815fcd8fd6627ef5
parunkov <parunkov@gmail.com>
2019-12-06 16:34:29 +0500

    ошибки

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 (function() {
+	let pictureBigOpen = false;
 	let picture = document.querySelector('.wp-image-3795');
 	let blockleft = document.querySelector('.blockleft');
 	let style = 'position: absolute; top: -100px; left:90px; width: 912px; height: 480px; background: url(' + picture.src + ') no-repeat; background-size: contain;';
-	picture.addEventListener('click', function(evt) {
+	let popupCloseStyle = 'font-size: 30px; text-align: right; padding-top: 10px; padding-right: 20px; cursor: pointer;';
+	// picture.addEventListener('click', function(evt) {
+	let onPictureClick = function(evt) {
 		evt.preventDefault();
-		let pictureBig = document.createElement('div');
+		// console.log('!!!');
+		pictureBig = document.createElement('div');
 		pictureBig.className = 'picture-big';
 		blockleft.prepend(pictureBig);
+		// pictureBigOpen = true;
 		blockleft.setAttribute('style', 'position: relative;');
 		pictureBig.setAttribute('style', style);
+
 		let popupClose = document.createElement('div');
 		popupClose.className = 'popup-close';
 		popupClose.innerHTML = 'Закрыть Х';
-		
-		
+		pictureBig.prepend(popupClose);
+		popupClose.setAttribute('style', popupCloseStyle);
+
+
+		popupClose.addEventListener('click', function(evt) {
+			evt.preventDefault();
+			pictureBig.remove();
+			picture.addEventListener('click', onPictureClick);
+			// document.removeEventListener('click', onClick);
+			pictureBigOpen = false;
+		});
+		picture.removeEventListener('click', onPictureClick);
+		pictureBigOpen = true;
+
+	
+	}
+	picture.addEventListener('click', onPictureClick);
+	document.addEventListener('click', function(evt) {
+		// console.log(pictureBigOpen);
+		console.log(evt.target != pictureBig);
+		console.log(pictureBig.length);
+		if (pictureBig.length) {
+			if (evt.target != pictureBig) {
+				pictureBig.remove();
+				picture.addEventListener('click', onPictureClick);
+				// document.removeEventListener('click', onClick);
+				pictureBigOpen = false;
+			}
+		}
 	});
+	
 })();
